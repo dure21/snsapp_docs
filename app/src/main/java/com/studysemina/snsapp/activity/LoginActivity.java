4fed7c4d5aaac0a5d1c1ae0b97e09f1046479a03
KJH <jinhungkim@hanmail.net>
2018-08-12 21:21:03 +0900

    NEW

--- app/src/main/java/com/studysemina/snsapp/activity/LoginActivity.java
+++ app/src/main/java/com/studysemina/snsapp/activity/LoginActivity.java
@@ -1,14 +1,58 @@
 package com.studysemina.snsapp.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.studysemina.snsapp.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 9001;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseUser user;
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userColRef = db.collection("user");
+
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +61,163 @@ public class LoginActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        Intent intent = new Intent(getApplication(), MainActivity.class);
-        startActivity(intent);
+        setupFirebaseAuth();
+
+        dialog = new ProgressDialog(LoginActivity.this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setMessage("로딩중..");
+        dialog.setCancelable(false);
+
+    }
+
+    private void setupFirebaseAuth() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        user = mAuth.getCurrentUser();
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    @OnClick(R.id.signInButton)
+    public void onSignInButton(View view) {
+        dialog.show();
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+
+                dialog.dismiss();
+            }
+        }
+    }
+
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            getUserInfo();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "구글인증 실패..",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    private void getUserInfo() {
+        user = mAuth.getCurrentUser();
+        userColRef.document(user.getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+                            Toast.makeText(LoginActivity.this, "로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+
+                            dialog.dismiss();
+
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+
+                        } else {
+
+                            writeUserInfo();
+                        }
+                    }
+                });
+
+    }
+
+    private void writeUserInfo() {
+
+
+        Map<String, Object> userMap = new HashMap<>();
+
+        if (user == null) return;
+
+        if (user.getEmail() != null) {
+            userMap.put("email", user.getEmail());
+        } else {
+            userMap.put("email", "temp@email.com");
+        }
+
+        if (user.getDisplayName() != null) {
+            userMap.put("nickname", user.getDisplayName());
+        } else {
+            userMap.put("nickname", "no nickname");
+        }
+
+        userColRef.document(user.getUid())
+                .set(userMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                        dialog.dismiss();
+
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "writeUserInfo failed", e);
+
+                        dialog.dismiss();
+
+
+                        Toast.makeText(LoginActivity.this, "writeUserInfo failed", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
         finish();
     }
 }
--- app/src/main/java/com/studysemina/snsapp/activity/MainActivity.java
+++ app/src/main/java/com/studysemina/snsapp/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.studysemina.snsapp.activity;
 
+import android.app.ProgressDialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,24 +16,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.studysemina.snsapp.R;
+import com.studysemina.snsapp.adapter.ChatAdapter;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference userColRef = db.collection("user");
+
+    private View navheaderView;
+    private String FirestoreNick;
+    private String FirestoreEmail;
+    private TextView nav_tv_GoogleNick;
+    private TextView nav_tv_GoogleEmail;
+    private TextView nav_tv_FirestoreNick;
+    private TextView nav_tv_FirestoreEmail;
+
+    private RecyclerView recyclerView;
+    private ChatAdapter adapter;
+
+    private ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         ButterKnife.bind(this);
 
+        setupUI();
 
+        dialog = new ProgressDialog(MainActivity.this);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        dialog.setMessage("등록중..");
+        dialog.setCancelable(false);
+
+
+    }
+
+    private void setupUI() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -38,7 +83,59 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navheaderView = navigationView.getHeaderView(0);
+        nav_tv_GoogleNick = (TextView) navheaderView.findViewById(R.id.nav_tv_GoogleNick);
+        nav_tv_GoogleEmail = (TextView) navheaderView.findViewById(R.id.nav_tv_GoogleEmail);
+        nav_tv_FirestoreNick = (TextView) navheaderView.findViewById(R.id.nav_tv_FirestoreNick);
+        nav_tv_FirestoreEmail = (TextView) navheaderView.findViewById(R.id.nav_tv_FirestoreEmail);
+
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+        if (user.getDisplayName() != null) {
+
+            nav_tv_GoogleNick.setText(user.getDisplayName());
+        }
+
+
+        if (user.getEmail() != null) {
+
+            nav_tv_GoogleEmail.setText(user.getEmail());
+        }
+
+        userColRef.document(user.getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if (documentSnapshot.exists()) {
+
+                            Log.d(TAG, "DocumentSnapshot data: " + documentSnapshot.getData());
+
+                            FirestoreNick = new String(documentSnapshot.getData().get("nickname").toString());
+                            FirestoreEmail = new String(documentSnapshot.getData().get("email").toString());
+
+                            if (FirestoreNick != null) {
+
+                                nav_tv_FirestoreNick.setText(FirestoreNick);
+                            }
+
+                            if (FirestoreEmail != null) {
+
+                                nav_tv_FirestoreEmail.setText(FirestoreEmail);
+                            }
+
+                        } else {
+
+                        }
+                    }
+                });
     }
+
+
 
     @Override
     public void onBackPressed() {
